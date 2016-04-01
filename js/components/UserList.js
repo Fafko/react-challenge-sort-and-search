176bc34cf25b50907eda6c765b52ac241d8ea7a0
Alex Panchenko <fafko93@gmail.com>
2016-04-02 00:25:49 +0300

    Added UserList component

---
+++ js/components/UserList.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import UserData from './UserData';
+
+export default
+class UserList extends Component{
+    
+    render(){
+
+        let users = [];
+
+        this.props.users.forEach((user) => {
+            users.push(<UserData user={user} key={user.id} />);
+        });
+        
+        return (
+            <div className='col-sm-8 col-md-9 col-lg-10' >
+                <table className='user-list table table-striped' >
+                    <thead>
+                        <tr>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>Age</th>
+                            <th>Phone</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {users}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+    
+}
